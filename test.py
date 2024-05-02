24caf6741bad0226deb52db44695f926692ab55e
Serena Xminds <serena@datazoom.io>
2024-05-02 17:49:41 +0530

    Print table number

---
+++ test.py
@@ -0,0 +1,40 @@
+import sys
+
+print("hello world");
+print(sys.version);
+
+x = 5;
+y = "String";
+
+print(type(x));
+print(type(y));
+
+x = "Test function";
+
+
+def myfun():
+    x = "new function";
+print("python with " + x);
+
+print(myfun())
+
+# Print table number
+
+n=int(input("Enter the number table for : "))
+for i in range(10):
+    print(n, "x",i, "=", n*i) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
