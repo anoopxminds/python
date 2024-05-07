74c43df4e3a4529f9b74257f1241b472cb0eb97e
Anoop Xminds <anoopak@xminds.com>
2024-05-07 12:56:30 +0530

    Find the last position of a given substring

---
+++ find_last_position_of _given_subst.py
@@ -0,0 +1,7 @@
+#Find the last position of a given substring
+
+str1 = "Find all occurrences of a substring in a given string by ignoring the case."
+
+position_of_last_string = str1.rfind("given"); 
+
+print("Last poition of substring : "+ str(position_of_last_string));
