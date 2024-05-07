4507958e460d1443498c67046460c78f74435b2e
Anoop Xminds <anoopak@xminds.com>
2024-05-07 10:58:40 +0530

    Code updated

--- find_average_for_given_number.py
+++ find_average_for_given_number.py
@@ -1,23 +1,12 @@
 #.Create a function in such a way that we can pass any number of integer numbers as arguments to this function, and the function should return the average of these integer numbers.
-list = []
-def take_numbers(default_range = 3):
-    for i in range(default_range):
-            number = input('Enter number for find avarege : ');
-            print(str(int(number)));
-            list.append(int(number));
-    return list;
 
-#print(take_numbers());
+def avg_fun(*num):
+      sum = 0;
+      for i in num:
+            sum = sum + i;
+      avg = sum/len(num);
+      print("Average : " + str(avg));
+      return avg;
 
-def findAverage(number):
-      if len(number) == 0 :
-            print("Enter a valid number");
-      else :  
-            average = len(number)/sum(number);
-            return average;
 
-def main() :
-      list = take_numbers();
-      print("Average of given number is : "+ str(findAverage(list)));
-
-main();
+avg_fun(2, 6, 4, 8);
