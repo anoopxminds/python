f9a362043776bc39857c3f7b404d1bbb5d514e3d
Anoop Xminds <anoopak@xminds.com>
2024-05-08 16:51:38 +0530

    Printing first two element from the given nested list

---
+++ print_last_2_elements_from_given_nested_list.py
@@ -0,0 +1,9 @@
+#Given a list [10,23,40,[9,3,4],6,7].Print the first 2 elements of sublist at position 3
+
+nested_list = [10,23,40,[9,3,4],6,7]
+
+# printing first two element 
+
+two_elements_from_sub_nested_list = nested_list[3][0:2]
+
+print(two_elements_from_sub_nested_list)
