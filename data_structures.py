31f32be4b29798519391a4a10073e3e536ca02d8
Anoop Xminds <anoopak@xminds.com>
2024-05-08 15:18:22 +0530

    data structures

---
+++ data_structures.py
@@ -0,0 +1,18 @@
+# data structures
+
+# tuple
+data_list = ('test', 'test1', 'test2')
+print(type(data_list))
+
+#set
+
+set_data = {"test1", "test2", "test3"}
+print(type(set_data))
+
+#Dictionary
+
+dictionary = {}
+print(type(dictionary))
+another_dict = dict()
+print(type(another_dict))
+
