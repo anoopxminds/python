44087df7903fe0fdc2a7c7a5ad24674b090a46b9
Anoop Xminds <anoopak@xminds.com>
2024-05-08 16:17:59 +0530

    Iterate through the following nested list using for loop and and print the elements

---
+++ print_nested_list_for_loop.py
@@ -0,0 +1,8 @@
+#Iterate through the following nested list using for loop and and print the elements
+
+nested_list = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+
+for sub_nested_list in nested_list:
+    for element in sub_nested_list:
+
+        print(element)
