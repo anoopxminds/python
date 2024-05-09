2d2cb6204f78913d4d7be315123beedf4a656764
Anoop Xminds <anoopak@xminds.com>
2024-05-09 10:49:31 +0530

    Assignment on Dictionary

--- assignment_on_dictionary.py
+++ assignment_on_dictionary.py
@@ -5,4 +5,36 @@ versions = [3, 22, 12]
 
 list = dict(zip(languages, versions))
 
-print(list)
+print("Dictionary list : ", list)
+
+
+# Delete some keys from the following dictionary
+
+empl_dict = {
+    "name": "Karthik",
+    "age": 25,
+    "salary": 80000,
+    "jobtitle": "Software developer"
+}
+
+print("Dictionary list : ", empl_dict)
+
+#Remiving salary 
+
+removed_key = "salary"
+
+print("The removing key is : " + str(removed_key))
+
+#Remiving key and value 
+
+removed_value = empl_dict.pop(removed_key, "Key is not found!")
+
+print("The removed key's value is : " + str(removed_value))
+
+print("Dictionary after removed key : ", str(empl_dict))
+
+# Rename the key jobtitle to position
+
+empl_dict['position'] = empl_dict.pop('jobtitle')
+
+print("Dictionary after change key : ", str(empl_dict))
