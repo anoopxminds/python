35f587ff88172e40e2aec29d4fb94f2880c43663
Anoop Xminds <anoopak@xminds.com>
2024-05-09 15:33:29 +0530

    python quize code update

--- python_quiz.py
+++ python_quiz.py
@@ -4,7 +4,7 @@ questions = [
     {
     'question': '1+1 ?',
     'options': [1, 2, 3, 'Skip', 'Exit'],
-    'answer': 1
+    'answer': 2
     },
     {
     'question': '5+1 ?',
@@ -42,7 +42,54 @@ for j, question in enumerate(questions):
         right_anwser +=1;
         print("Correct");   
     else :
-        print("Wrong !, Correct answer is ", question['answer'])
+        print(f"Wrong !, Correct answer is {question['answer']}")
         
-    print("Your score is ", right_anwser , " out of ", len(questions))
+    print(f"Your score is {right_anwser}/{len(questions)}")
+
+
+
+
+# Second way
+
+print("--------------Second solution ------------------")
+
+quiz = {
+    '1+1 ?': {
+    'options': (1, 2, 3),
+    'answer': 2
+    },
+    '5+1 ?': {
+    'options': (5, 6, 8),
+    'answer': 6
+    },
+    '10 - 5 ?': {
+    'options': (5, 10, 2),
+    'answer': 5
+    },
+    '1 * 5 ?': {
+    'options': (5, 10, 15),
+    'answer': 5
+    }
+}
+
+i = 0;
+score  = 0
+for q in quiz:
+    i = i+1
+    print(f"Question {i}. {q}")
+    _options = quiz[q]['options']
+    print(f"Choose an option {_options}")
+    _user_answer = int(input("Choose your answer:"))
+    _answer = quiz[q]['answer']
+    if(_user_answer == _answer) :
+        score +=1;
+        print("Your answer is correct")
+    else:
+        print("Wrong answer.")
+        
+    print(f"Your score is {score}/{len(quiz)}")
+    
+    
+    
+    
 
