17d2e18b6d2a61b7037628759872c75430370d03
Anoop Xminds <anoopak@xminds.com>
2024-05-15 16:27:03 +0530

    Assignment on OOPS #1 and #2

--- assignment_on_oops_#1.py
+++ assignment_on_oops_#1.py
@@ -17,3 +17,54 @@ print(books.id)
 print(books.author)
 print(books.name)
 print(books.publish_date)
+
+print("=============================================================================")
+
+class Student:
+    pass_mark  = 300
+    college_name = "Collage of Engineering"
+    def __init__(self, name, roll_number, total_mark):
+        self.name = name
+        self.roll_number = roll_number
+        self.total_mark = total_mark
+        
+    def getMark(self):
+        status = "Failed"
+        if self.pass_mark <= self.total_mark:
+            status = "Passed"
+        return status
+        
+    
+
+student1 = Student("Student 1", "001", 200)
+student2 = Student("Student 2", "002", 100)
+student3 = Student("Student 3", "003", 300)
+student4 = Student("Student 4", "004", 400)
+
+print("============= Status of Student 1 ======================")
+
+print(f"Name : {student1.name} ")
+print(f"Collage : {student1.college_name}")
+print(f"status: {student1.getMark() }")
+print(f"Achived mark: {student1.total_mark}")
+
+print("============= Status of Student 2 ======================")
+
+print(f"Name : {student2.name} ")
+print(f"Collage : {student2.college_name}")
+print(f"status: {student2.getMark() }")
+print(f"Achived mark: {student2.total_mark}")
+
+print("============= Status of Student 3 ======================")
+
+print(f"Name : {student3.name} ")
+print(f"Collage : {student3.college_name}")
+print(f"status: {student3.getMark() }")
+print(f"Achived mark: {student3.total_mark}")
+
+print("============= Status of Student 4 ======================")
+
+print(f"Name : {student4.name} ")
+print(f"Collage : {student4.college_name}")
+print(f"status: {student4.getMark() }")
+print(f"Achived mark: {student4.total_mark}")
