4d90abffa08894cc400fb08fabd0103854069f00
Anoop Xminds <anoopak@xminds.com>
2024-05-23 12:51:09 +0530

    Body - Update replacing with PUT and Partial updates with PATCH

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from fastapi.encoders import jsonable_encoder
 import uvicorn
 from enum import Enum
 from pydantic import BaseModel
 from typing import Optional
+from typing import List, Union
+from typing_extensions import Annotated
 
 app = FastAPI()
 
@@ -33,6 +36,7 @@ class Item(BaseModel):
     description: Optional[str] = None
     price:float
     tax: Optional[float] = None
+    tag: List[str] = []
 
 @app.post("/items")
 async def postItems(item: Item):
@@ -41,6 +45,56 @@ async def postItems(item: Item):
         price_with_tax = item.price * item.tax
         item_dict.update({"Price with Tax": price_with_tax})
     return item_dict
+
+
+@app.put("/items/{item_id}")
+async def updateItem(item_id: int, item:Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.get("/items")
+async def read_items(
+    q: Annotated[
+        Union[str, None],
+        Query(
+            alias= "item-query",
+            title= "Query String",
+            description= "Query string item for the search in database that have a good match",
+            min_length= 3,
+            max_length= 10
+        )
+    ] = None
+):
+    result = {"items": [{"item_id": "Foo"}, {"Item_id": "Bar"}]}
+    if q:
+        result.update({"q": q})
+    return result
+    
+    
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.get("/items_list/{item_id}", response_model=Item)
+async def read_item_list(item_id: str):
+    return items[item_id]
+    
+@app.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id:str, item:Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.dict(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
+    
+    
+
 
 
 if __name__ == "__main__":
